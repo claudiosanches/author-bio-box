6b6d8a7efff21d41d89b202900a576b7879254be
claudiosmweb <contato@claudiosmweb.com>
2013-12-13 18:18:49 -0200

    updated the translation

--- public/class-author-bio-box.php
+++ public/class-author-bio-box.php
@@ -259,11 +259,11 @@ class Author_Bio_Box {
 	 * @return void
 	 */
 	public function load_plugin_textdomain() {
-
 		$domain = self::get_plugin_slug();
 		$locale = apply_filters( 'plugin_locale', get_locale(), $domain );
 
-		load_textdomain( $domain, trailingslashit( WP_LANG_DIR ) . $domain . '/' . $domain . '-' . $locale . '.mo' );
+        load_textdomain( $domain, trailingslashit( WP_LANG_DIR ) . $domain . '/' . $domain . '-' . $locale . '.mo' );
+        load_plugin_textdomain( $domain, FALSE, basename( plugin_dir_path( dirname( __FILE__ ) ) ) . '/languages/' );
 	}
 
 	/**
