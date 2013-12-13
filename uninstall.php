d4c40cdafd87b546067c249e4326b2cb2df4d14c
claudiosmweb <contato@claudiosmweb.com>
2013-12-13 17:30:39 -0200

    improved the uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -1,9 +1,15 @@
 <?php
+/**
+ * Fired when the plugin is uninstalled.
+ *
+ * @package   Author_Bio_Box
+ * @author    Claudio Sanches <contato@claudiosmweb.com>
+ * @license   GPL-2.0+
+ * @copyright 2013 Claudio Sanches
+ */
 
-// If uninstall not called from WordPress exit.
-if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) || ! WP_UNINSTALL_PLUGIN || dirname( WP_UNINSTALL_PLUGIN ) != dirname( plugin_basename( __FILE__ ) ) ) {
-
-	status_header( 404 );
+// If uninstall not called from WordPress, then exit
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
 
